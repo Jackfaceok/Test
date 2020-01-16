a97c8783a997b1c6e6b0043c7b229fbd3ef5e3c2
xxl <xxl1786556@163.com>
2020-01-16 16:46:07 +0800

    first

---
+++ exercise/src/main/java/Test1.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName Test1
+ * @Description TODO
+ * @Author 夏晓林
+ * @Date 2020/1/16 16:41
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("这是测试一");
+    }
+}
