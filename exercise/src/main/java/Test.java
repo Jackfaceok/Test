cfe99c23073c41fc7aad83a7e1b5507ed567484e
xxl <xxl1786556@163.com>
2020-01-16 16:37:48 +0800

    Hello File

---
+++ exercise/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 夏晓林
+ * @Date 2020/1/16 16:35
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是一个test测试");
+    }
+
+}
